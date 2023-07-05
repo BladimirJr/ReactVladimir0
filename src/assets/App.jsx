2395db4be6565d114f1cbaba74a09f8826bb84a1
Vladimir Jr. Estrada <99416700+BladimirJr@users.noreply.github.com>
2023-07-05 11:11:00 -0500

    first commit

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -8,6 +8,9 @@ export function App () {
     <CardFollow  userName="Bladimir_Jr_" InitialIsFollowing={false} >
     Vladimir
     </CardFollow>
+    <CardFollow  userName="Bladimir_Jr_" InitialIsFollowing={false} >
+    Vladimir
+    </CardFollow>
     </section>
     
     )
